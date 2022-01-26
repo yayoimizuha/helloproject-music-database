c4a0e9f3cc24d272ba2158d8ed180c2888ee8ea1
yayoimizuha <poko.peko.tomo@icloud.com>
2022-01-27 02:19:45 +0900

    improve parse_uf_release.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 import search_on_youtube
 from contextlib import redirect_stdout
 
-result = search_on_youtube.search_on_youtube(search_keyword="大きな愛でもてなして", artist_keyword='℃-ute',
+result = search_on_youtube.search_on_youtube(search_keyword="だけど会いたい", artist_keyword='Bitter & Sweet',
                                              api_key='AIzaSyC4rd76PUGRfPZlcS9q-28VB1nV34mZv4Y', use_itunes_search=True)
 
 pprint.pprint(result, indent=4)
--- parse_uf_release.py
+++ parse_uf_release.py
@@ -1,4 +1,5 @@
 # http://www.up-front-works.jp/release/
+import html
 import pprint
 from search_on_itunes import safe_request_get_as_text
 from bs4 import BeautifulSoup
@@ -6,11 +7,22 @@ import requests
 
 release_page_url = "http://www.up-front-works.jp/release/search/?g=single&-s=1"
 
-page_text = requests.get(release_page_url).text
+page_text = safe_request_get_as_text(release_page_url)
 
-pprint.pprint(page_text)
+# pprint.pprint(page_text)
 
 page_obj = BeautifulSoup(page_text, 'html.parser')
-release_list = page_obj.find('div', {'id': 'release_list'})
-pprint.pprint(release_list, indent=4)
-print(len(release_list))
+
+release_list = page_obj.find('div', {'id': 'release_list'}).find_all('a', {'class', 'box'})
+
+for i in range(len(release_list)):
+    print(release_list[i].find('img')['alt'])
+    print('http://www.up-front-works.jp' + release_list[i]['href'])
+    release_info = safe_request_get_as_text('http://www.up-front-works.jp' + release_list[i]['href'])
+    release_info_obj = BeautifulSoup(release_info, 'html.parser').find('div', {'id': 'right'}).find_all('table')
+    # print(html.unescape(release_info_obj.prettify()))
+    for j in range(len(release_info_obj)):
+        print(html.unescape(release_info_obj[j].find_previous().text))
+        print(str(i) + '\t' + str(j))
+
+pprint.pprint(release_list)
