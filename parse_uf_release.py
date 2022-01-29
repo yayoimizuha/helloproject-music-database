f3108f1060a9c03fa2a706a80b5b51997009c396
片山　朋和 <poko.peko.tomo@icloud.com>
2022-01-29 14:09:28 +0900

    convert pandas dataflame & export Excel file

--- parse_uf_release.py
+++ parse_uf_release.py
@@ -1,9 +1,11 @@
 # http://www.up-front-works.jp/release/
+import datetime
 import html
 import pprint
 from search_on_itunes import safe_request_get_as_text
 from bs4 import BeautifulSoup
 import requests
+import pandas
 
 release_url = "http://www.up-front-works.jp/release/search/?-s=1&g=single&p=10"
 
@@ -53,11 +55,14 @@ def road_release_page(url):
 
 # road_table(road_release_page(road_release_list(release_url)))
 
-# release_list_urls = ["http://www.up-front-works.jp/release/search/?-s=1&g=single&p=1"]
 release_list_urls = []
-for i in range(39):
-    print("http://www.up-front-works.jp/release/search/?-s=1&g=single&p=" + str(i + 1))
-    release_list_urls.append("http://www.up-front-works.jp/release/search/?-s=1&g=single&p=" + str(i + 1))
+release_music_list = []
+
+for page_num in range(39):
+    print("http://www.up-front-works.jp/release/search/?-s=1&g=single&p=" + str(page_num + 1))
+    release_list_urls.append("http://www.up-front-works.jp/release/search/?-s=1&g=single&p=" + str(page_num + 1))
+
+# release_list_urls = ["http://www.up-front-works.jp/release/search/?-s=1&g=single&p=1"]
 
 for release_list_url in release_list_urls:
     release_list_items = road_release_list(release_list_url)
@@ -67,8 +72,9 @@ for release_list_url in release_list_urls:
         release_page_tables = road_release_page(release_list_item)
 
         for release_page_table in release_page_tables:
-
             print("**************************************************")
+
+            print(release_list_item[0])
             for column in release_page_table.find_all('tr'):
                 if not column.has_attr('class'):
                     if column.find('p') is None:
@@ -93,3 +99,17 @@ for release_list_url in release_list_urls:
                           column.find('td', {'class': 'columnD'}).text + '\t' +
                           column.find('td', {'class': 'columnE'}).text + '\t' +
                           column.find('td', {'class': 'columnF'}).text)
+                    release_music_list.append([release_list_item[1].split(':')[0],
+                                               release_list_item[1].split(':')[1].replace('\t', ''),
+                                               column.find('td', {'class': 'columnB'}).text,
+                                               release_list_item[0]])
+
+# pprint.pprint(release_music_list)
+dataflame = pandas.DataFrame(release_music_list, columns=['artist', 'collectionName', 'trackName', 'release_page'])
+pandas.options.display.max_rows = None
+pandas.options.display.max_columns = None
+pandas.options.display.width = 6000
+pandas.options.display.max_colwidth = 6000
+
+# print(dataflame)
+dataflame.to_excel(str(datetime.date.today()) + '.xlsx')
